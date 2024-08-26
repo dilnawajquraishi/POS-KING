30cf33609d7f17dfc6fef3f2e6e08efcb1e0532c
dilnawajquraishi <dilnawajquraishi@gmail.com>
2024-08-26 12:58:57 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,82 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const db = require("./dataBaseConfig.js")
+
+const cors = require("cors")
+const dotenv = require("dotenv")
+
+
+dotenv.config()
+
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({extended: true, limit: "100kb"}))
+app.use(express.static('uploads'))
+app.use(cors())
+
+db().then(async () => {
+
+   
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    })
+
+
+    const productRoute = require('./routes/productRoute.js')
+
+    const salesReport = require('./routes/salesReport.js')
+
+
+    const categoryRoute = require('./routes/categoryRoute.js')
+
+    const employeeRoute = require('./routes/addEmployeeRoute.js')
+
+    const addressRoute = require('./routes/addAddressRoute.js')
+
+    const administratorRoute = require('./routes/administratorRoute.js')
+
+    const ProductReportRoute = require('./routes/ProductReport.js')
+
+    const PosRoute = require('./routes/posRoutes.js')
+
+    const purchage = require('./routes/productRoute.js')
+
+    const SaleDetails = require('./routes/SalesDetailsRoute.js')
+
+
+
+    
+
+    app.use('/api', productRoute)
+
+
+    app.use('/api', categoryRoute)
+
+
+    app.use('/api', employeeRoute)
+
+
+    app.use('/api', administratorRoute)
+
+
+    app.use('/api', addressRoute)
+
+
+    app.use('/api', salesReport)
+
+
+    app.use('/api', ProductReportRoute)
+
+    app.use('/api', PosRoute)
+
+    app.use('/api',     purchage)
+
+
+    app.use('/api',     SaleDetails)
+
+
+
+    
+
+})
+.catch(err => console.log(err))
